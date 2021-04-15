4fae14e0d4169d04f6b555baefa7e936cf62f8e8
Yudai Watanabe <yudai.0702wa@gmail.com>
2021-04-15 23:58:59 +0900

    cardにInkWellでリップルエフェクトをかけた

--- tarkov_task_manager/lib/main.dart
+++ tarkov_task_manager/lib/main.dart
@@ -32,62 +32,127 @@ class TaskListPage extends StatelessWidget {
         children: <Widget>[
           // CardとListTileを使い、簡単に整ったUIを作成
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/prapor.jpg', width: 50),
-              title: Text('Prapor'),
+            //InkWellでリップルエフェクトをかける
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/prapor.jpg', width: 50),
+                  title: Text('Prapor'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/therapist.jpg', width: 50),
-              title: Text('Therapist'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/therapist.jpg', width: 50),
+                  title: Text('Therapist'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/fence.jpg', width: 50),
-              title: Text('Fence'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/fence.jpg', width: 50),
+                  title: Text('Fence'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/skier.jpg', width: 50),
-              title: Text('Skier'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/skier.jpg', width: 50),
+                  title: Text('Skier'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/peacekeeper.jpg', width: 50),
-              title: Text('Peacekeeper'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/peacekeeper.jpg', width: 50),
+                  title: Text('Peacekeeper'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/mechanic.jpg', width: 50),
-              title: Text('Mechanic'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/mechanic.jpg', width: 50),
+                  title: Text('Mechanic'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/ragman.jpg', width: 50),
-              title: Text('Ragman'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/ragman.jpg', width: 50),
+                  title: Text('Ragman'),
+                ),
+              ),
             ),
           ),
 
           Card(
-            child: ListTile(
-              leading: Image.asset('assets/images/jaeger.jpg', width: 50),
-              title: Text('Jaeger'),
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/jaeger.jpg', width: 50),
+                  title: Text('Jaeger'),
+                ),
+              ),
             ),
           ),
+
+          Card(
+            //InkWellでリップルエフェクトをかける
+            child: InkWell(
+              onTap: () => debugPrint("Card tapped"),
+              child: Padding(
+                padding: EdgeInsets.all(1.0),
+                child: ListTile(
+                  leading: Image.asset('assets/images/prapor.jpg', width: 50),
+                  title: Text('Prapor'),
+                ),
+              ),
+            ),
+          ),
+
         ],
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // "push"で新規画面に遷移
