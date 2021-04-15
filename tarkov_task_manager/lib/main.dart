662c9b99ca770f89fc23ea2c1e3246acc9d59e3b
Yudai Watanabe <yudai.0702wa@gmail.com>
2021-04-15 23:48:49 +0900

    assetsに画像をセットし、cardにトレーダーのアイコンを表示

--- tarkov_task_manager/lib/main.dart
+++ tarkov_task_manager/lib/main.dart
@@ -33,48 +33,56 @@ class TaskListPage extends StatelessWidget {
           // CardとListTileを使い、簡単に整ったUIを作成
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/prapor.jpg', width: 50),
               title: Text('Prapor'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/therapist.jpg', width: 50),
               title: Text('Therapist'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/fence.jpg', width: 50),
               title: Text('Fence'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/skier.jpg', width: 50),
               title: Text('Skier'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/peacekeeper.jpg', width: 50),
               title: Text('Peacekeeper'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/mechanic.jpg', width: 50),
               title: Text('Mechanic'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/ragman.jpg', width: 50),
               title: Text('Ragman'),
             ),
           ),
 
           Card(
             child: ListTile(
+              leading: Image.asset('assets/images/jaeger.jpg', width: 50),
               title: Text('Jaeger'),
             ),
           ),
